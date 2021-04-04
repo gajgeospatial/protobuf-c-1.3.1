441661db0e629309848f2450cc4b23017a05707c
gajgeospatial <glen.johnson@gaj-geospatial.com>
2021-04-04 19:01:22 -0400

    Initial checkin of windows build

---
+++ msvc/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake
+# Build directory: C:/Development/op3d_active/protobuf-c-1.3.1/msvc
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-generated-code "test-generated-code")
+set_tests_properties(test-generated-code PROPERTIES  _BACKTRACE_TRIPLES "C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;143;ADD_TEST;C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;0;")
+add_test(test-generated-code2 "test-generated-code2")
+set_tests_properties(test-generated-code2 PROPERTIES  _BACKTRACE_TRIPLES "C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;144;ADD_TEST;C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;0;")
+add_test(test-generated-code3 "test-generated-code3")
+set_tests_properties(test-generated-code3 PROPERTIES  _BACKTRACE_TRIPLES "C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;145;ADD_TEST;C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;0;")
+add_test(test-issue220 "test-issue220")
+set_tests_properties(test-issue220 PROPERTIES  _BACKTRACE_TRIPLES "C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;146;ADD_TEST;C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;0;")
+add_test(test-issue251 "test-issue251")
+set_tests_properties(test-issue251 PROPERTIES  _BACKTRACE_TRIPLES "C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;147;ADD_TEST;C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;0;")
+add_test(test-version "test-version")
+set_tests_properties(test-version PROPERTIES  _BACKTRACE_TRIPLES "C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;148;ADD_TEST;C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake/CMakeLists.txt;0;")
---
+++ msvc/cmake_install.cmake
@@ -0,0 +1,76 @@
+# Install script for directory: C:/Development/op3d_active/protobuf-c-1.3.1/build-cmake
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/protobuf-c")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Release")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Is this installation the result of a crosscompile?
+if(NOT DEFINED CMAKE_CROSSCOMPILING)
+  set(CMAKE_CROSSCOMPILING "FALSE")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/Debug/protoc-gen-c.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/Release/protoc-gen-c.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/MinSizeRel/protoc-gen-c.exe")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/RelWithDebInfo/protoc-gen-c.exe")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/Debug/protobuf-c.lib")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/Release/protobuf-c.lib")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/MinSizeRel/protobuf-c.lib")
+  elseif("${CMAKE_INSTALL_CONFIG_NAME}" MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
+    file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib" TYPE STATIC_LIBRARY FILES "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/RelWithDebInfo/protobuf-c.lib")
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/protobuf-c" TYPE FILE FILES "C:/Development/op3d_active/protobuf-c-1.3.1/protobuf-c/protobuf-c.h")
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES "C:/Development/op3d_active/protobuf-c-1.3.1/protobuf-c/protobuf-c.h")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "C:/Development/op3d_active/protobuf-c-1.3.1/msvc/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
